364240f959119e16a8f281c1d1cf1cd0adf14ad3
kurasa-satu <76093496+kurasa-satu@users.noreply.github.com>
2021-05-31 16:06:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,7 @@ const au3 = ["https://au3.glitch.me"]
 const au4 = ["https://au4.glitch.me"]
 
 setInterval(function() {
-
-}
+ au1.forEach(h => {
+  axios.get(h).then(console.log("Kapten logs"))
+ })
+},100*1000)
