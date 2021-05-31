a7cfb22292ea42635f08cff77ba1c369fdf67198
kurasa-satu <76093496+kurasa-satu@users.noreply.github.com>
2021-05-31 16:02:02 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const axios = require("axios")
+const au1 = ["https://au1.glitch.me"]
+const au2 = ["https://au2.glitch.me"]
+const au3 = ["https://au3.glitch.me"]
+const au4 = ["https://au4.glitch.me"]
+
+setInterval(function() {
+
+}
